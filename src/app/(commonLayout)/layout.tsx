ca90d722fdf6956591cee63d3450ce0c6f510923
abujaher5 <abujaher5120@gmail.com>
2026-02-05 23:09:39 +0600

    implement navbar and navlinks

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,5 +1,12 @@
+import { Navbar } from "@/components/layout/Navbar";
+
 const CommonLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
 };
 
 export default CommonLayout;
